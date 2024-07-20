5f91d5e945eb2ff69913554048b76bb6d56b539e
dev_uoon <104570636+devuoon@users.noreply.github.com>
2024-07-20 16:59:49 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,10 +1,6 @@
 function solution(my_string) {
-    let stringRep = my_string.replace(/[a-z]/gi, 0);
-    let arr = [...stringRep];
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        let num = parseInt(arr[i]);
-        sum += num;
-    }
-    return sum;
+    let answer = my_string.replace(/[a-z]/gi, 0)
+                    .split('')
+                    .reduce((acc,cur)=>acc + (parseInt(cur) || 0), 0);
+    return answer
 }
