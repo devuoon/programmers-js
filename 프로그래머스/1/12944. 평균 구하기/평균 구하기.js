f44ecb51190029c76778b27d725cbb2764cfbf34
dev_uoon <104570636+devuoon@users.noreply.github.com>
2024-07-24 03:45:49 +0900

    [level 1] Title: 평균 구하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -1,10 +1,5 @@
 function solution(arr) {
     // 1. 배열의 값 더하기 / 배열 값 갯수
-    var answer = 0;
-    let sum = 0;
-    for(i=0; i<arr.length; i++){
-        sum += arr[i]
-        answer = sum / arr.length;
-    }
+    var answer = arr.reduce((arr,cur) => arr + cur)/arr.length;
     return answer;
 }
